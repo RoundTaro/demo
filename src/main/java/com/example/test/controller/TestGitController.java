1231d3328b1683fbfd798a052c489b698cdd9cfc
RoundTaro <m17863153370@163.com>
2025-06-30 01:04:37 +0800

    test git

--- src/main/java/com/example/test/controller/TestGitController.java
+++ src/main/java/com/example/test/controller/TestGitController.java
@@ -1,7 +1,17 @@
 package com.example.test.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class TestGitController {
+    /**
+     * 访问 testIndex.html 页面
+     * @return 视图名，对应 testIndex.html
+     */
+    @GetMapping("/index")
+    public String showTestIndexPage() {
+        System.out.println("访问 testIndex.html 页面");
+        return "testIndex.html";
+    }
 }
